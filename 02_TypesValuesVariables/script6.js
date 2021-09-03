7ba5bf0e0d75272f74b7e071c633a8831c8d0ef2
Wichanart <wcnsk15@gmail.com>
2021-09-03 13:49:03 +0700

    [add] zodiac.js

--- 02_TypesValuesVariables/script6.js
+++ 02_TypesValuesVariables/script6.js
@@ -1,7 +1,7 @@
 //let, const, var
 //const can't be updated but let and var can be updated
-//cost and let can't be re-declared but var can re-declared :declare คือการประกาศค่า
-//cost like final static in java it's can't change value
+//const and let can't be re-declared but var can re-declared :declare คือการประกาศค่า
+//const like final static in java it's can't change value
 
 /* var x //declare 
 x=10; //defualt is var assign คือการกำหนดค่า */
@@ -10,8 +10,11 @@ x=10; //defualt is var assign คือการกำหนดค่า */
 
 //var with global scope, under window object ex.window.std
 var std = 10;
-var std = `myStudent`; //re-declared std variable สร้างตัวแปรใหม่ใช้ชื่อเดิม
+var std = `myStudent`; //re-declared std variable สร้างตัวแปรใหม่ใช้ชื่อเดิม กำหนดพื้นที่ในaddress memoryใหม่แล้วตั้งชื่อตัวแปร
 //can re-assign use = 
+//but update คือตัวแปรที่อ้างถึงaddress memไหน ก็อัปเดตที่เดิม เปลี่ยน value
+//พยายามใช้ let, comsole
+//string can use  '' single quote " " double quote and `` backtick 
 
 function doSomething() {
     //var with local scope can use only in {}
