21b46f239013409b769165775f6af2ee427ee777
Wichanart <wcnsk15@gmail.com>
2021-09-17 16:24:13 +0700

    [update] arrays

--- INT201_test/Arrays.js
+++ INT201_test/Arrays.js
@@ -18,14 +18,28 @@ doSomething(arr);
 console.log(arr); //[ 2, 1998, 6, 8 ]
 
 
-let student = [
+let students = [
     {id: 1, name: 'Ann'},
     {id: 2, name: 'Peter'},
     {id: 3, name: 'Mary'}
 ]
-console.log(student[0]);
-student[student.length-1].name = 'Wichanart'; //ระบุเข้าไป
-console.log(student[student.length-1]);
+console.log(students[0]);
+students[students.length-1].name = 'Wichanart'; //ระบุเข้าไป
+console.log(students[students.length-1]);
+
+let students2=[1, ...students, 100]; //... follow by array
+console.log(students2.length); //5
+console.log(students2); 
+//  1,
+//{ id: 1, name: 'Ann' },
+//{ id: 2, name: 'Peter' },
+//{ id: 3, name: 'Wichanart' },
+//100
+students2[1].name = 'XYZ'; //ชื่ออเรย์[ตำแหน่งที่1].ชื่อตัวแปร เปลี่ยนเป็น XYZ
+console.log(students2[1]); 
+console.log(students)
+console.log(students2);
+
 
 let colors = [
     [`pink`, `red`],
@@ -34,3 +48,4 @@ let colors = [
 console.log(colors[1][colors[1].length-1]) //find the last value for 2 มิติ
 console.log(colors[0][colors[0].length - 1]);
 console.log(colors[colors.length - 1][colors[1].length - 1]);
+
